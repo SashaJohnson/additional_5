78e69c39e709f68df0c8ca0f1d22f14db39d8a92
SashaJohnson <alexjohnson@yandex.ru>
2018-09-24 15:19:30 +0300

    implemented checker for one bracket type: ()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  /*
+  var configList = [];
+  for (var i = 0; i < configList.length; i++) {
+	  
+	*/
+	
+	var stub = bracketsConfig;
+	
+	var count = 0;
+	
+	for (var i = 0; i < str.length; i++) {
+		
+		if (str[i] == "(") {
+			
+			count++;
+			
+		}
+		
+		else if (str[i] == ")") {
+				
+				if (count == 0) {
+					
+					return false;
+					
+				} else {
+				
+					count--;
+				
+			}
+			
+	}
+	
+
+  
+    if (count == 0) {
+	  
+	    return true;
+	  
+    } else {
+	  
+	    return false 
+	
+	    }
+  
+	}
+
 }
