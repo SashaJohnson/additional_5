3d18afde2722580872b67d065734adf7d6cc61bd
SashaJohnson <alexjohnson@yandex.ru>
2018-09-25 01:15:20 +0300

    implemented check for (), [], {}

--- src/index.js
+++ src/index.js
@@ -1,39 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  /*
-  var configList = [];
-  for (var i = 0; i < configList.length; i++) {
-	  
-	*/
+ 
+    var stub = bracketsConfig;
 	
-	var stub = bracketsConfig;
-	
-	var count = 0;
-	
-	for (var i = 0; i < str.length; i++) {
-		
-		if (str[i] == "(") {
-			
-			count++;
-			
-		}
-		
-		else if (str[i] == ")") {
-				
-			count--;
-				
-			}
-		
-	}
-	
-  
-    if (count == 0) {
-	  
-	    return true;
-	  
+    var closeBrackets = [')', ']', '}'];
+    var bracketPairs = '()[]{}';
+    var stack = [];
+    
+    for (var i = 0; i < str.length; i++) {
+    
+    if (closeBrackets.indexOf(str[i]) != -1) {
+       
+        if (stack == []) {return false};
+            
+            var last = stack.pop();
+            var position = bracketPairs.indexOf(str[i]);
+       
+            if(last != bracketPairs[position - 1]) {return false};
+        
     } else {
-	  
-	    return false 
-	
-	    }
-  
-	}
+        stack.push(str[i]);
+    }
+
+}  
+
+if (stack.length == 0) {return true;}
+    else {return false;}
+}
+
